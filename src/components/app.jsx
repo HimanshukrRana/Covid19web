3a3013aca62469600ac7336301af3346152d2b74
Himanshu Rana <hr.himanshu143@gmail.com>
2022-08-25 16:53:15 +0530

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Static from "./static";
+import Cases from "./cases";
+import Last from "./last";
+const App = () => {
+  return (
+    <>
+      <React.Fragment>
+        <Static />
+        <Cases />
+        <Last />
+      </React.Fragment>
+    </>
+  );
+};
+
+export default App;
